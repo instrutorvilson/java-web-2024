0b1c1fc39219adad15fa8ba09684a046d69f9756
Vilson Moro <vilson.moro@PROWAY.TREINA>
2024-02-04 15:53:22 -0300

    Criado endpoint de inserir e consultar contatos

--- Api/src/main/java/com/aulaapi/Api/controller/ContatoController.java
+++ Api/src/main/java/com/aulaapi/Api/controller/ContatoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,22 @@ public class ContatoController {
 	public String tchau() {
 		return "arriverdi";
 	}*/
+	
+	@GetMapping("/contatos/{idcontato}")
+	public  ResponseEntity<?> consultarUm(@PathVariable("idcontato")
+	Long idcontato){
+		Contato contato = null;
+		for(Contato ct : contatos) {
+			if(ct.getId() == idcontato) {
+				contato = ct;
+				break;
+			}
+		}
+		if (contato != null) {
+		   return ResponseEntity.status(HttpStatus.OK).body(contato);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("message: contato não encontrado");
+	}
 	
 	@GetMapping("/contatos")
 	public  ResponseEntity<?> consultarTodos(){
