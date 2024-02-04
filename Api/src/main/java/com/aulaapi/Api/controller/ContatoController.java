f48f49b3b2fa42fd3789c4735adb35d8c8ec6492
Vilson Moro <vilson.moro@PROWAY.TREINA>
2024-02-04 17:34:32 -0300

    Implementar endpoint de exclusão

--- Api/src/main/java/com/aulaapi/Api/controller/ContatoController.java
+++ Api/src/main/java/com/aulaapi/Api/controller/ContatoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,17 @@ public class ContatoController {
 			return ResponseEntity.status(HttpStatus.OK).body(con);
 		}
 		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("message: contato não encontrado");
+	}
+	
+	@DeleteMapping("/contatos/{idcontato}")
+	public  ResponseEntity<?> excluir(@PathVariable("idcontato")
+	Long idcontato){
+		Contato contato = existeContato(idcontato);
+		if (contato != null) {
+		   contatos.remove(contato);	
+		   return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("message: contato não encontrado");
 	}
 
